c822a8db12d019ab360bfd856eca20972e242977
tyson <tyelf22@hotmail.com>
2020-03-31 21:37:04 -0600

    added search ability

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
       <!-- Apollo watched Graphql query -->
     <ApolloQuery
-      :query="require('../graphql/HelloWorld.gql')"
-    >
+      :query="require('../graphql/HelloWorld.gql')" 
+      :variables="{searchString}" >
       <template v-slot="{ result: { loading, error, data } }">
         <!-- Loading -->
         <div v-if="loading" class="loading apollo">Loading...</div>
@@ -12,11 +12,33 @@
 
         <!-- Result -->
         <div v-else-if="data" class="result apollo">
-          <v-list>
-            <v-list-item v-for="(player, i) in data.Players" :key="i" > 
-              <p>{{player.firstname}}</p>  
-            </v-list-item>
-          </v-list>
+          <v-row>
+            <v-col v-for="(player, i) in data.Players" :key="i">
+              <v-card
+                class="mx-auto"
+                width="420"
+              >
+                <v-card-text>
+                  <p class="display-1 text--primary">
+                    {{player.firstname}} {{player.lastname}}
+                  </p>
+                  <p>{{player.team}}</p>
+                  <div class="text--primary">
+                    Height: {{player.height}} <br/>
+                    Weight: {{player.weight}}
+                  </div>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn
+                    text
+                    color="deep-purple accent-4"
+                  >
+                    Learn More
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
         </div>
 
         <!-- No result -->
@@ -30,7 +52,13 @@
     name: 'HelloWorld',
 
     data: () => ({
-
+      searchString: ""
     }),
   }
 </script>
+
+<style scoped>
+  .result {
+    padding: 1rem;
+  }
+</style>
